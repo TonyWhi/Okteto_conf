96f9db39a3981dba513a76cd977c10bf2399e685
TonyWhi <97328499+TonyWhi@users.noreply.github.com>
2022-08-08 20:30:00 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-apt install iputils-ping -y
+apt install iputils-ping wget -y
 
-ping www.baidu.com
+cd /root/save
+
+if [ ! -f ttygo ];
+then
+  wget -O ttygo.tar.gz https://github.com/yudai/gotty/releases/download/v2.0.0-alpha.3/gotty_2.0.0-alpha.3_linux_amd64.tar.gz
+  tar -zxf ttygo.tar.gz
+  mv gotty ttygo
+fi
+./ttygo -p 25001 -w -c wjm:okteto0529 bash
+
 
 bash
