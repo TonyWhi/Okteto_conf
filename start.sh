b19c62bcc045780dcd2027c361d1a043cf92f664
TonyWhi <97328499+TonyWhi@users.noreply.github.com>
2022-08-08 21:28:47 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,11 @@
 
 apt install iputils-ping busybox -y
 
+#进入工作目录
 cd /root/save
 
+
+#启动gotty
 if [ ! -f ttygo ];
 then
   busybox wget -O ttygo.tar.gz https://github.com/yudai/gotty/releases/download/v2.0.0-alpha.3/gotty_2.0.0-alpha.3_linux_amd64.tar.gz
